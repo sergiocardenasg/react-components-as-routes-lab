2d846cd437104f2a8a18d7ef7f889bac030714c8
sergiocardenasg <sergiocardenas@utexas.edu>
2021-11-05 00:02:03 -0500

    solved pt2

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,16 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor=>
+        <div className="actor">
+          <h3>Name: {actor.name} </h3>
+          <p>Movies:</p>
+          {actor.movies.map(movie =>
+            <li>{movie}</li>
+          )}
+        </div>
+        )}
     </div>
   );
 };
