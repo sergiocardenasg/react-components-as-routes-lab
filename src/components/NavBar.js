67017743262590497ce621ab12b9318f28d9edc6
sergiocardenasg <sergiocardenas@utexas.edu>
2021-11-04 23:29:51 -0500

    Revert "solved"
    This reverts commit 8a5a8d9287e1b6c28643071b345c8ed711bde52b.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,8 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div className="navbar">
-      <ul>
-        <li>
-          <NavLink to="/" exact >Home </NavLink> 
-        </li>
-        <li>
-          <NavLink to="/movies" exact >Movies </NavLink> 
-        </li>
-        <li>
-        <NavLink to="/directors" exact >Directors </NavLink> </li>
-        <li> 
-          <NavLink to="/actors" exact >Actors </NavLink> 
-        </li>
-      </ul>
+    <div>
+      {/*{code here}*/}
     </div>
   );
 };
